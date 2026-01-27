2acc2293cf8bfcffafb10b58a4f5edc4ffa4d64e
Alex Fantini <fa@fantinialex.com>
2026-01-27 22:16:07 +0100

    Refactor group fetching and membership checking to support pagination

--- src/helpers/outline.py
+++ src/helpers/outline.py
@@ -13,28 +13,75 @@ outline_client = AsyncOutline(
     base_url=os.getenv('OUTLINE_URL')
 )
 
-async def get_outline_groups():
+async def get_outline_groups() -> list:
     outline_groups = []
-    # Getting all Outline groups
-    outline_groups_response = await outline_client.post(path='/api/groups.list?limit=100', cast_to=httpx.Response)
-    outline_groups_json = json.loads(await outline_groups_response.aread())
-    for group in outline_groups_json['data']['groups']:
-        outline_groups.append({group['name']: group['id']})
+    offset = 0
+    limit = 100
+    has_more_groups = True
+    
+    # Handle pagination to get all groups
+    while has_more_groups:
+        # Fetch groups
+        outline_groups_response = await outline_client.post(
+            path=f'/api/groups.list', 
+            cast_to=httpx.Response,
+            body={'limit': limit, 'offset': offset}
+        )
+        logger.debug(f"Fetching groups with offset {offset}")
+
+        outline_groups_json = json.loads(await outline_groups_response.aread())
+        groups = outline_groups_json['data']['groups']
+        
+        # Determine if there are more groups to fetch
+        has_more_groups = len(groups) == limit
+
+        # Append groups to the list
+        for group in groups:
+            outline_groups.append({group['name']: group['id']})
+
+        # Increment offset for next batch
+        offset += limit
 
     logger.info(f"Got {len(outline_groups)} groups from Outline")
-    return(outline_groups)
+    return (outline_groups)
 
 async def get_group_membership(group_id, user_id, user_name):
-    outline_memberships_response = await outline_client.post(path='/api/groups.memberships?limit=100', cast_to=httpx.Response, body={'id': group_id, 'query': user_name})
-    outline_memberships_json = json.loads(await outline_memberships_response.aread())
+    offset = 0
+    limit = 100
+    has_more_users = True
+    
+    # Handle pagination to get all group memberships
+    while has_more_users:
+        # Fetch group memberships
+        outline_memberships_response = await outline_client.post(
+            path=f'/api/groups.memberships', 
+            cast_to=httpx.Response, 
+            body={
+                'id': group_id,
+                'query': user_name,
+                limit: limit,
+                offset: offset
+            }
+        )
+        logger.debug(f"Fetching memberships for group {group_id} with offset {offset}")
 
-    # Check in each of the [data][users] if user_id is in any of them
-    for user in outline_memberships_json['data']['users']:
-        if user['id'] == user_id:
-            logger.debug(f"User {user_name} is in group {group_id}")
-            return(True)
+        outline_memberships_json = json.loads(await outline_memberships_response.aread())
+        users = outline_memberships_json['data']['users']
+        
+        # Determine if there are more users to fetch
+        has_more_users = len(users) == limit
+
+        # Check in each of the [data][users] if user_id is in any of them
+        for user in users:
+            if user['id'] == user_id:
+                logger.debug(f"User {user_name} is in group {group_id}")
+                return (True)
+        
+        # Increment offset for next batch
+        offset += limit
+    
     logger.debug(f"User {user_name} is not in group {group_id}")
-    return(False)
+    return (False)
 
 async def add_user_to_group(group_id, user_id):
     response = await outline_client.post(path='/api/groups.add_user', cast_to=httpx.Response, body={'id': group_id, 'userId': user_id})
